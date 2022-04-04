297f3c2e9795ad898f4bcb4dabedd91dead28a25
AD!TYA <88816438+AdityaOP2008@users.noreply.github.com>
2022-04-04 13:19:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+ var c = require('chalk');
+module.exports = {
+    subtract,
+    divide,
+    multiply,
+    add,
+    logcyan,
+    logred,
+    logblue, 
+    loggreen,
+    logyellow
+}
+
+ function logcyan(text) {
+  return console.log(c.cyan(text));
+ }
+function logyellow(text) {
+  return console.log(c.yellow(text));
+}
+function loggreen(text) {
+  return console.log(c.green(text));
+}
+function logred(text) {
+  return console.log(c.red(text));
+}
+function logblue(text) {
+  return console.log(c.blue(text));
+}
+ 
+function add(a, b) {
+    return a + b;
+}
+ 
+function subtract(a, b) {
+    return a - b;
+}
+ 
+function divide(a, b) {
+    return a / b;
+}
+ 
+function multiply(a, b) {
+    return a * b;
+}
